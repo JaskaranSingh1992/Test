4cc0aea301031d4e0046c0b1b9822a0455bfea7c
Jaskaran Singh <jaskaransingh.nic@gmail.com>
2022-08-26 17:24:09 +0530

    push1

--- WebApplications/WebApplications/Controllers/WeatherForecastController.cs
+++ WebApplications/WebApplications/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplications.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Jaskaran"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
